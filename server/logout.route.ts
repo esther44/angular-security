5d6893312e02a67bdf03d75d4ed6c84ad76041c4
esther44 <esther.desbois@gmail.com>
2021-12-10 11:38:35 +0100

    Add logout message

--- server/logout.route.ts
+++ server/logout.route.ts
@@ -9,7 +9,7 @@ export function logout(req: Request, res: Response){
 
   res.clearCookie('SESSIONID');
 
-  res.clearCookie( "XSRF-TOKEN");
+  res.clearCookie( 'XSRF-TOKEN');
 
-  res.status(200).json({});
+  res.status(200).json({message: 'Logout Successful'});
 }
